e6f8de3574f463e59926faca439c5cc06baaf998
Guilherme Padilha <gs.padilha@gmail.com>
2023-12-25 11:30:02 -0300

    feat: primeiro controller para Demo

--- src/main/java/br/dev/gspadilha/basejavaspringboot/BaseJavaSpringBootApplication.java
+++ src/main/java/br/dev/gspadilha/basejavaspringboot/BaseJavaSpringBootApplication.java
@@ -2,8 +2,10 @@ package br.dev.gspadilha.basejavaspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.dev.gspadilha")
 public class BaseJavaSpringBootApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/dev/gspadilha/demo/PrimeiroController.java
@@ -0,0 +1,61 @@
+package br.dev.gspadilha.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@RestController
+@RequestMapping("/primeiroController")
+public class PrimeiroController {
+
+    record User(String name, String pass) {
+    }
+
+    @GetMapping("/primeiroMethod")
+    public String primeiroMethod() {
+        return "Você chegou em algum lugar no demo!";
+    }
+
+    @GetMapping("/methodWithParameters/{id}")
+    public String methodWithParameters(@PathVariable String id) {
+        return "O parâmetro informado é " + id + "!";
+    }
+
+    @GetMapping("/methodWithQueryParams")
+    public String methodWithQueryParams(@RequestParam String id) {
+        return "O parâmetro informado é " + id + "!";
+    }
+
+    @GetMapping("/methodWithMultipleQueryParams")
+    public String methodWithMultipleQueryParams(@RequestParam Map<String, String> params) {
+        return "O parâmetro informado é " + params.entrySet() + "!";
+    }
+
+    @PostMapping("/methodWithBody")
+    public String methodWithBody(@RequestBody User user) {
+        return "O usuário é " + user.name() + " com senha" + user.pass() + "!";
+    }
+
+    @PostMapping("/methodWithHeaders")
+    public String methodWithHeaders(@RequestHeader Map<String, String> params) {
+        return "Os headers enviados são " + params.entrySet() + "!";
+    }
+
+    @PostMapping("/methodWithResponseEntity")
+    public ResponseEntity<Object> methodWithResponseEntity() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Request Not Found ");
+    }
+
+}
