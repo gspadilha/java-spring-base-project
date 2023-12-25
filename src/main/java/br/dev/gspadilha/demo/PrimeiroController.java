bc41d1bbc2d9c6636efd0dffa4fb3580b1a2066a
Guilherme Padilha <gs.padilha@gmail.com>
2023-12-25 11:33:08 -0300

    feat: ajustes

--- src/main/java/br/dev/gspadilha/demo/PrimeiroController.java
+++ src/main/java/br/dev/gspadilha/demo/PrimeiroController.java
@@ -2,13 +2,11 @@ package br.dev.gspadilha.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,7 +53,7 @@ public class PrimeiroController {
 
     @PostMapping("/methodWithResponseEntity")
     public ResponseEntity<Object> methodWithResponseEntity() {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Request Not Found ");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Request Not Found");
     }
 
 }
