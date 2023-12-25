36e54bb2bd53af4947bca8a295f7a4d5907d0483
Guilherme Padilha <gs.padilha@gmail.com>
2023-12-25 09:58:54 -0300

    feat: first commit

---
+++ src/main/java/br/dev/gspadilha/basejavaspringboot/BaseJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.dev.gspadilha.basejavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseJavaSpringBootApplication.class, args);
+	}
+
+}
